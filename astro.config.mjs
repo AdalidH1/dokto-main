c4e0dd603b5acef0a344cc440af700b53d18abd2
Mafe-SL <fernanda.mariastb@gmail.com>
2023-12-05 17:44:16 -0600

    v5

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,7 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   integrations: [
     tailwind()
-  ]
+  ],server: {
+    port: 8000, // El puerto que deseas utilizar
+  },
 });
